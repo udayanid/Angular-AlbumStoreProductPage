9b116170d6c2ed2a8703cea5a68ca7b27a424a19
udayanid <udhayan.addon@gmail.com>
2020-02-21 01:59:43 +0530

    dyamic tracklisting load using ngfor  with new component

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Album } from '../album';
+import { ProductService } from 'app/product.service';
+
+@Component({
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+  private albumInfo: Album;
+  constructor(private _productService: ProductService) {
+  }
+
+  ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
+  }
+
+}
